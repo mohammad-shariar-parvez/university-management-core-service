fdb11fdd317c8a914721a193165746362b81d2b8
Piaowei <msptonmoy@gmail.com>
2023-09-22 12:37:51 +0600

    feat(prisma): complete prisma migration

---
+++ prisma/migrations/20230914204243_student_enrllment/migration.sql
@@ -0,0 +1,30 @@
+-- CreateEnum
+CREATE TYPE "StudentEnrolledCourseStatus" AS ENUM ('ONGOING', 'COMPLETED', 'WITHDRAWN');
+
+-- AlterTable
+ALTER TABLE "academic_semesters" ADD COLUMN     "isCurrent" BOOLEAN DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "student_enrolled_courses" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "courseId" TEXT NOT NULL,
+    "academicSemesterId" TEXT NOT NULL,
+    "grade" TEXT,
+    "point" DOUBLE PRECISION DEFAULT 0,
+    "totalMarks" INTEGER DEFAULT 0,
+    "status" "StudentEnrolledCourseStatus" DEFAULT 'ONGOING',
+
+    CONSTRAINT "student_enrolled_courses_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "student_enrolled_courses" ADD CONSTRAINT "student_enrolled_courses_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_enrolled_courses" ADD CONSTRAINT "student_enrolled_courses_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "student_enrolled_courses" ADD CONSTRAINT "student_enrolled_courses_academicSemesterId_fkey" FOREIGN KEY ("academicSemesterId") REFERENCES "academic_semesters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
